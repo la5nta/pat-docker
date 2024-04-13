b4b35e4c32be713f9650755e2ef31f429b863a7a
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2024-04-13 12:05:00 +0200

    Fix VARA FM exec script

--- .services/vara/varafm.sh
+++ .services/vara/varafm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 envsubst < asoundrc.subst > ~/.asoundrc
-tail -F -n0 .wine/drive_c/VARA\ FM/VARAFM.log & wineserver -k; xvfb-run wine .wine/drive_c/VARA\ FM/VARAFM.exe
+tail -F -n0 /wine/drive_c/VARA\ FM/VARAFM.log & wineserver -k; xvfb-run wine /wine/drive_c/VARA\ FM/VARAFM.exe
