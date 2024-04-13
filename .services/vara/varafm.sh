f7c74903fa99cfd077ac9b0c818357502da2e0d6
Martin Hebnes Pedersen <martin.h.pedersen@gmail.com>
2024-04-13 12:25:57 +0200

    Fix issue restarting VARA containers

--- .services/vara/varafm.sh
+++ .services/vara/varafm.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 envsubst < asoundrc.subst > ~/.asoundrc
-tail -F -n0 /wine/drive_c/VARA\ FM/VARAFM.log & wineserver -k; xvfb-run wine /wine/drive_c/VARA\ FM/VARAFM.exe
+tail -F -n0 /wine/drive_c/VARA\ FM/VARAFM.log &
+rm -f /tmp/.X*-lock # Work-around for dirty termination of Xvfb
+wineserver -k; xvfb-run wine /wine/drive_c/VARA\ FM/VARAFM.exe
--- .services/vara/varahf.sh
+++ .services/vara/varahf.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 envsubst < asoundrc.subst > ~/.asoundrc
-tail -F -n0 /wine/drive_c/VARA/VARAHF.log & wineserver -k; xvfb-run wine /wine/drive_c/VARA/VARA.exe
+tail -F -n0 /wine/drive_c/VARA/VARAHF.log &
+rm -f /tmp/.X*-lock # Work-around for dirty termination of Xvfb
+wineserver -k; xvfb-run wine /wine/drive_c/VARA/VARA.exe
